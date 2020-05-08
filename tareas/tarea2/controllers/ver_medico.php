7547c09017cb13cdce4667feb4c42869b1e55463
Joaquin-aliaga <joaliaga.g@gmail.com>
2020-05-08 00:44:25 -0400

    agrega ver_medico

---
+++ tareas/tarea2/controllers/ver_medico.php
@@ -0,0 +1,45 @@
+<?php
+require_once('../models/db_config.php');
+$db = DbConfig::getConnection();
+
+$id_medico = $_GET["id"];
+
+$query = "SELECT M.id, M.nombre, M.experiencia, C.nombre as comuna, M.twitter, M.email, M.celular 
+FROM medico as M, comuna as C 
+WHERE M.id = $id_medico AND M.comuna_Id = C.Id";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Datos medico</title>
+	<link rel="stylesheet" type="text/css" href="../css/tarea1.css">
+    <link type="text/css" rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" />
+    
+</head>
+<body>
+<?php
+$result = $db->query($query);
+if ($result->num_rows > 0){
+    echo "<table class=table><tbody><tr><th>Nombre Médico</th><th>Experiencia</th><th>Comuna</th><th>Twitter</th>
+    <th>Email</th><th>Celular</th></tr>\n";
+	
+	while ($row = $result->fetch_row()) {
+		echo "\t<tr >\n";
+        echo "\t<td>$row[1]</td>\n"; //nombre
+        echo "\t<td>$row[2]</td>\n"; //experiencia
+		echo "<td>$row[3]</td>\n"; //id comuna
+        echo "<td>$row[4]</td>"; //Twitter
+        echo "<td>$row[5]</td>"; //Mail
+        echo "<td>+$row[6]</td>\n"; //Celular
+		echo "\t</tr>\n";
+	}
+	echo "</table>\n";
+	echo "</tbody>";
+    echo "</table>";
+}
+?>  
+<a href="./ver_medicos.php">Volver a lista de médicos</a>
+</body>
+</html>
--- tareas/tarea2/controllers/ver_medicos.php
+++ tareas/tarea2/controllers/ver_medicos.php
@@ -2,8 +2,10 @@
 require_once('../models/db_config.php');
 $db = DbConfig::getConnection();
 
-$query = "SELECT id, nombre, experiencia, comuna_id, twitter, email, celular FROM medico ORDER BY id DESC LIMIT 5";
-
+$query = "SELECT M.id, M.nombre, M.experiencia, C.nombre as comuna, twitter, M.email, M.celular 
+FROM medico as M, comuna as C 
+WHERE M.comuna_Id = C.Id 
+ORDER BY id DESC LIMIT 5";
 ?>
 
 <!DOCTYPE html>
@@ -11,10 +13,10 @@ $query = "SELECT id, nombre, experiencia, comuna_id, twitter, email, celular FRO
 <head>
 	<title>Ver médicos disponibles</title>
 	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="./css/tarea1.css">
+	<link rel="stylesheet" type="text/css" href="../css/tarea1.css">
     <link type="text/css" rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="./js/tarea1.js"></script>
+    <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="./js/tarea1.js"></script> -->
 </head>
 <body>
 
@@ -22,13 +24,14 @@ $query = "SELECT id, nombre, experiencia, comuna_id, twitter, email, celular FRO
 $result = $db->query($query);
 if ($result->num_rows > 0){
 	// FALTA AGREGAR <th>Especialidades</th>
-	echo "<table class=table><tbody><tr><th>Nombre Médico</th><th>Comuna</th><th>Datos Contacto</th></tr>\n";
+	echo "<table class=table><tbody><tr><th></th><th>Nombre Médico</th><th>Comuna</th><th>Datos Contacto</th></tr>\n";
 	
 	while ($row = $result->fetch_row()) {
-		echo "\t<tr>\n";
+		echo "\t<tr >\n";
+		echo "<td><a href=ver_medico.php?id=$row[0]>Ver médico</a></td>\n";
 		echo "\t<td>$row[1]</td>\n"; //nombre
-		echo "<td>$row[3]</td>\n"; //id comuna
-		echo "<td>twitter: $row[4] <br> mail: $row[5] <br> celular: $row[6]</td>\n";
+		echo "<td>$row[3]</td>\n"; //comuna
+		echo "<td>twitter: $row[4] <br> mail: $row[5] <br> celular: +$row[6]</td>\n";
 		echo "\t</tr>\n";
 	}
 	echo "</table>\n";
